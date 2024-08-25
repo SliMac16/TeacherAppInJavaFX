17f28235e19f479784b901d5306aeea727f2178c
Maciej Śliwiński <maciej.sliwinski98@gmail.com>
2024-08-25 22:47:45 +0200

    cteach

---
+++ src/main/java/com/example/fx3/Controllers/CreateTeacherController.java
@@ -0,0 +1,33 @@
+package com.example.fx3.Controllers;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class CreateTeacherController {
+
+
+    @FXML
+    private ComboBox<?> cb_Condition;
+
+    @FXML
+    private ComboBox<?> cb_Group;
+
+    @FXML
+    private DatePicker dp_Date;
+
+    @FXML
+    private TextField tb_Name;
+
+    @FXML
+    private TextField tb_Salary;
+
+    @FXML
+    private TextField tb_Surname;
+
+
+
+}
